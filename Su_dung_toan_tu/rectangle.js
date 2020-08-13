4d2c65a193e9d6c2490de3a109812f5265babafe
thanhdt246 <dothanh24062000@gmail.com>
2020-08-13 18:58:35 +0700

    edit practice1

--- Su_dung_toan_tu/rectangle.js
+++ Su_dung_toan_tu/rectangle.js
@@ -1,12 +1,14 @@
 function rectangle(){
-    let inputWidth = prompt("Enter the width: ");
-    let inputHeight = prompt("Enter the height: ");
+    let width = parseInt(prompt("Enter the width: "));
+    let height = parseInt(prompt("Enter the height: "));
 
-    let width = parseInt(inputWidth);
-    let height = parseInt(inputHeight);
-
-    document.getElementById("width").innerHTML = "Entered width: " + width;
-    document.getElementById("height").innerHTML = "Entered height: " + height;
+    while(width <=0 || height<=0){
+        alert("Error! Try again");
+        width = parseInt(prompt("Enter the width: "));
+        height = parseInt(prompt("Enter the height: "));
+    }
+    document.getElementById("width").innerHTML = "Width: " + width;
+    document.getElementById("height").innerHTML = "Height: " + height;
     let area = width * height;
     document.getElementById("area").innerHTML = "The area is:  " + area;
 }
