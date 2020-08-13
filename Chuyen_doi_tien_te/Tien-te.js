22923127ebdf4ce99ded0478c57ccddd2a7b36c7
thanhdt246 <dothanh24062000@gmail.com>
2020-08-13 19:24:45 +0700

    edit exercise

--- Chuyen_doi_tien_te/Tien-te.js
+++ Chuyen_doi_tien_te/Tien-te.js
@@ -2,5 +2,9 @@ function Convert(){
     let amount = parseFloat(document.getElementById("amount").value);
     let from = parseFloat(document.getElementById("from").value);
     let to = parseFloat(document.getElementById("to").value);
-    document.getElementById("result").innerHTML = "Result: " + amount * from/to;
+    if(amount<0){
+        document.getElementById("result").innerHTML = "Error";
+    }else{
+        document.getElementById("result").innerHTML = "Result: " + amount * from/to;
+    }
 }
